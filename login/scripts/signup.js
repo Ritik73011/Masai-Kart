830b4e374473e868e4a45890c67798666e36f833
Sheikhzain07 <sheikhzain459@gmail.com>
2022-10-29 02:19:21 +0530

    responsive product description page completed

--- login/scripts/signup.js
+++ login/scripts/signup.js
@@ -1,56 +1,30 @@
 import { ref, onValue, set, remove, database, update } from "../../config.js";
 
-import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "../../config.js";
+import { getAuth, createUserWithEmailAndPassword } from "../../config.js";
 
-
-function onSignInSubmit() {
-
-    document.getElementById("btn2").addEventListener("click", () => {
-        // console.log("hello")
-        // const appVerifier = window.recaptchaVerifier;
-        // const phoneNum = "+16505551234";
-        // const auth = getAuth();
-        // signInWithPhoneNumber(auth, phoneNum, appVerifier)
-        //     .then((confirmationResult) => {
-        //         // SMS sent. Prompt user to type the code from the message, then sign the
-        //         // user in with confirmationResult.confirm(code).
-        //         window.confirmationResult = confirmationResult;
-        //         // ...
-        //         alert("hello")
-        //     }).catch((error) => {
-        //         // Error; SMS not sent
-        //         // ...
-        //         console.log(error)
-        //     });
-
-    })
+document.getElementById("btn2").addEventListener("click",()=>{
+    let email=document.getElementById("email").value;
+    let password=document.getElementById("password").value;
+    createUser(email,password);
+})
+function createUser(email,password) {
+console.log(email,password)
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            alert(user);
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage)
+            // ..
+        });
 }
 
-const auth = getAuth();
-// window.recaptchaVerifier = new RecaptchaVerifier('captcha', {}, auth);
-window.recaptchaVerifier = new RecaptchaVerifier('captcha', {
-    'size': 'normal',
-    'callback': (response) => {
-        const appVerifier = window.recaptchaVerifier;
-        const phoneNum = "+16505551234";
-        // const auth = getAuth();
-        signInWithPhoneNumber(auth, phoneNum, appVerifier)
-            .then((confirmationResult) => {
-                // SMS sent. Prompt user to type the code from the message, then sign the
-                // user in with confirmationResult.confirm(code).
-                window.confirmationResult = confirmationResult;
-                // ...
-                alert("hello")
-            }).catch((error) => {
-                // Error; SMS not sent
-                // ...
-                console.log(error)
-            });
-    },
-    'expired-callback': () => {
-      // Response expired. Ask user to solve reCAPTCHA again.
-      // ...
-    }
-  }, auth);
+
 
 
