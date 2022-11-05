86041566a2fb2390c59f231ca79df80b00fc903f
Sheikhzain07 <sheikhzain459@gmail.com>
2022-11-05 23:40:57 +0530

    profile page's order list updated

--- Profile/profile.js
+++ Profile/profile.js
@@ -3,17 +3,51 @@ import { footer } from "../footer/footer.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "../config.js";
 import { ref, onValue, set, remove, database, update } from "../config.js";
 
+
+let auth = getAuth();
+onAuthStateChanged(auth, (isLogin) => {
+    if (isLogin) {
+        let userUid = isLogin.uid;
+        onValue(ref(database, "userInfo" + "/" + userUid), (uidObj) => {
+            let data = uidObj.val();
+            document.getElementById("name").value = data.name;
+            document.getElementById("email").value = data.email;
+            document.getElementById("phone").value = "11111111";
+
+        })
+
+        document.getElementById("update").addEventListener("click",()=>{
+            let name1=document.getElementById("name").value;
+            let email1=document.getElementById("email").value;
+            update(ref(database,"userInfo/"+ userUid),{
+                name: name1,
+                email: email1
+            })
+            document.getElementById("name").setAttribute("readonly",true);
+            document.getElementById("email").setAttribute("readonly",true);
+        })
+        getOrders(userUid);
+    }
+})
+
+document.getElementById("edit").addEventListener("click",()=>{
+    document.getElementById("name").removeAttribute("readonly");
+    document.getElementById("email").removeAttribute("readonly");
+    
+})
+
+
+
 document.getElementById("navbar").innerHTML = navBarHtml();
 navBarJavaScript();
 document.getElementById("footer").innerHTML = footer();
 
-appendOrders();
-appendWishList();
+// appendWishList();
 
 
-function appendOrders() {
+function appendOrders(ele) {
     //document.getElementById("orders").innerHTML = "";
-    for (let i = 0; i < 10; i++) {
+        console.log(ele.img1)
         let mainD = document.createElement("div");
         mainD.id = "gridDiv";
 
@@ -22,27 +56,27 @@ function appendOrders() {
 
         let imgD = document.createElement("div");
         let img = document.createElement("img");
-        img.src = "https://ca.slack-edge.com/T036SUFAE1H-U03MYABQE15-5dfb0935c950-72";
+        img.src = ele.img1;
         imgD.append(img);
 
         let title = document.createElement("p");
         title.id = "pTitle";
-        title.innerText = "Zain Sheikh";
+        title.innerText = ele.title;
 
         let price = document.createElement("p");
         price.id = "pPrice";
-        price.innerText = "10cr";
+        price.innerText = ele.price;
 
         itemDiv.append(imgD, title, price);
         mainD.append(itemDiv);
 
         document.getElementById("orders").append(mainD);
-    }
+    
 }
 
-function appendWishList() {
+function appendWishList(ele) {
     //document.getElementById("orders").innerHTML = "";
-    for (let i = 0; i < 10; i++) {
+    
         let mainD = document.createElement("div");
         mainD.id = "gridDiv";
 
@@ -51,22 +85,22 @@ function appendWishList() {
 
         let imgD = document.createElement("div");
         let img = document.createElement("img");
-        img.src = "https://ca.slack-edge.com/T036SUFAE1H-U03MYABQE15-5dfb0935c950-72";
+        img.src = ele.img1;
         imgD.append(img);
 
         let title = document.createElement("p");
         title.id = "pTitle";
-        title.innerText = "Zain";
+        title.innerText = ele.title;
 
         let price = document.createElement("p");
         price.id = "pPrice";
-        price.innerText = "10cr";
+        price.innerText = ele.price;
 
         itemDiv.append(imgD, title, price);
         mainD.append(itemDiv);
 
         document.getElementById("wishlist").append(mainD);
-    }
+    
 }
 
 addEvent("orders1", "orders", "userInfo", "wishlist", "grid", "none", "userInfo1", "wishlist1");
@@ -84,5 +118,32 @@ function addEvent(id, idm, id2, id3, str1, str2, bg1, bg2) {
         document.getElementById(id).style.backgroundColor = "lightgrey";
         document.getElementById(bg1).style.backgroundColor = "rgb(245, 244, 244)";
         document.getElementById(bg2).style.backgroundColor = "rgb(245, 244, 244)";
+    })
+}
+
+function getOrders(uid){
+    const starCountRef = ref(database, "orderHistory/" + uid);
+        onValue(starCountRef, (snapshot) => {
+            const data = snapshot.val();
+            let obj=Object.keys(data);
+            fetchOrders(obj,uid)
+        })
+
+}
+
+function fetchOrders(arr,uid){
+    arr.map(ele=>{
+        onValue(ref(database, "orderHistory/" + uid+"/"+ ele),(snapshot)=>{
+            const data=snapshot.val();
+            appendOrders(data);
+        });
+    })
+}
+function fetchWishlist(arr,uid){
+    arr.map(ele=>{
+        onValue(ref(database, "wishlist/" + uid+"/"+ ele),(snapshot)=>{
+            const data=snapshot.val();
+            appendWishList(data);
+        });
     })
 }
--- login/scripts/signup.js
+++ login/scripts/signup.js
@@ -3,18 +3,25 @@ import { ref, onValue, set, remove, database, update } from "../../config.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "../../config.js";
 
 document.getElementById("btn2").addEventListener("click", () => {
+    let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
-    createUser(email, password);
+    createUser(name, email, password);
 })
-function createUser(email, password) {
+function createUser(name, email, password) {
     console.log(email, password)
     const auth = getAuth();
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed in 
             const user = userCredential.user;
+            const myUid = user.uid;
+            set(ref(database, "userInfo/" + myUid), {
+                name: name,
+                email: email
+            })
             alert(user);
+            // console.log(user.uid);
             // ...
         })
         .catch((error) => {
